49061f2cc4572727ad4f333aad8c1ace3c8a8feb
Vuyo Dila <103577606+D3konR3kon@users.noreply.github.com>
2025-06-01 18:26:13 +0200

    updated folder structure

--- alx_travel_app/alx_travel_app/listings/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import shared_task
-from .models import Listing
-
-@shared_task
-def process_listing_data():
-    listings_count = Listing.objects.count()
-    return f"Processed {listings_count} listings"
